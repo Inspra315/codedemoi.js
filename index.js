36d9cbcfc74a910bd1de494b9fed032017e78587
Nilaax <55708705+Nilaax@users.noreply.github.com>
2020-05-24 04:21:34 +0200

    Create index.js
    index.js changed

---
+++ index.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js")
+const Enmap = require("enmap")
+const fs = require("fs")
+
+const bot = new Discord.Client();
+const config = require("./config.json");
+bot.config = config;
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    bot.on(eventName, event.bind(null, bot));
+  });
+});
+
+bot.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    bot.commands.set(commandName, props);
+  });
+});
+
+bot.login(config.token);
