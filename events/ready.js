6ac71d04164c27699fce6484e8d3823487387369
Nilaax <55708705+Nilaax@users.noreply.github.com>
2020-05-24 05:14:34 +0200

    Create ready.js
    ok gros

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+module.exports = (client, message) => {
+  console.clear()
+  console.log(`[+] LitanianBOT connecté avec succès !`)
+  client.user.setActivity("Se fait développer par Inspra#6139",{type: 'PLAYING'
+  })
+  };
